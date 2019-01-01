e1238f26800671548325a6f3d4b227bf0a3bd318
Tri Nanda <zidanecr7kaka@gmail.com>
2019-01-01 17:04:24 +0700

    profile page and avatars

---
+++ microblog.py
@@ -0,0 +1,14 @@
+from app import app, db
+from app.models import User, Post
+
+
+# (venv) $ pip install python-dotenv
+# FLASK_APP=microblog.py
+
+
+#ketika flask sell dijalankan, ini akan menjalankan fungsi ini, dan mendaftarkan item yang di return olehnya kedalam session sell
+@app.shell_context_processor
+def make_shell_contex():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
