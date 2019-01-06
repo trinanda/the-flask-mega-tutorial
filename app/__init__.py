6cbdbe4eb4fd2ddeabbd3b4f9fa952777281cf64
Tri Nanda <zidanecr7kaka@gmail.com>
2019-01-06 17:40:09 +0700

    Email support
    Took 46 minutes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
@@ -15,6 +16,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 
 if not app.debug:
@@ -46,3 +48,5 @@ if not app.debug:
 
 
 from app import routes, models, errors
+
+
